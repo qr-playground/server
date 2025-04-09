03fccdad6660c90eea0186bda92ae3f479e66e07
su-hwani <wjdtnghks123@kookmin.ac.kr>
2025-04-09 22:37:15 +0900

    Init Project For Start

--- src/main/java/com/example/demo/domain/user/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.domain.user.entity;
-
-public enum Role {
-    ADMIN, 
-    NORMAL,
-}
--- src/main/java/com/example/demo/global/exception/AlreadyReturnedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.global.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class AlreadyReturnedException extends BaseException {
-    public AlreadyReturnedException(String message) {
-        super(message, HttpStatus.CONFLICT);
-    }
-}
